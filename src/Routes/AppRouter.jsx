8a477640c7f8da2d067492abeaeec5f6dacc6742
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-23 13:21:38 -0600

    add a nav bar for the example

--- src/Routes/AppRouter.jsx
+++ src/Routes/AppRouter.jsx
@@ -1,14 +1,38 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { Login } from "../components/Login";
 import { Landing } from "../components/Landing";
 import { Dashboard } from "../components/Dashboard";
 import { Analize } from "../components/Analize";
 import { Home } from "../components/Home";
+import '../../src/App.css';
 
 export const RouterApp = ()=>{
     return(
         <BrowserRouter>
+        <nav>
+            <ul>
+                <li>
+                    <Link className="link" to="/landing">Landing</Link>
+                </li>
+
+                <li>
+                    <Link className="link" to="/home">Home</Link>
+                </li>
+
+                <li>
+                    <Link className="link" to="/login">Login</Link>
+                </li>
+
+                <li>
+                    <Link className="link"  to="/dashboard">Dashboard</Link>
+                </li>
+
+                <li>
+                    <Link className="link" to="/analize">Analize</Link>
+                </li>
+            </ul>
+        </nav>
         <Routes>
             <Route index element={<Login />} />
 
